8132d82be7268413310981e46ade0c821849b861
Riship Gupta <guptariship@gmail.com>
2023-06-10 11:55:46 +0530

    Add useState and grab advice on click of button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 
 function App() {
+  // Setting useState to grab the advice everytime we click button
+  const [advice, setAdvice] = useState("");
 
   async function getAdvice() {
     const obj = await fetch("https://api.adviceslip.com/advice");
     const data = await obj.json();
     console.log(data);
+    setAdvice(data.slip.advice);
   }
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Click to get advice!!</h1>
+      <h2>{advice}</h2>
       <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
