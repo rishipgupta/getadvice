b120f3985e7efcfd6fdfa656852802ae08033989
Riship Gupta <guptariship@gmail.com>
2023-06-10 12:00:58 +0530

    Set number of times the button is clicked

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 function App() {
   // Setting useState to grab the advice everytime we click button
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const obj = await fetch("https://api.adviceslip.com/advice");
     const data = await obj.json();
     // console.log(data);
     setAdvice(data.slip.advice);
+    setCount(count + 1);
   }
 
   return (
@@ -16,6 +18,7 @@ function App() {
       <h1>Click to get advice!!</h1>
       <h2>{advice}</h2>
       <button onClick={getAdvice}>Get Advice</button>
+      <h3>Number of times you asked for an advice: {count}</h3>
     </div>
   );
 }
